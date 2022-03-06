9fb59fb50f370ffdf03d6b5e7e4026f787903b7a
Robson <joserobsonsiqueira23@hotmail.com>
2022-03-06 16:59:29 -0300

    correções de bug

--- src/components/reposContainer/index.jsx
+++ src/components/reposContainer/index.jsx
@@ -11,7 +11,7 @@ const ReposContainer = props => {
             <Title>Repositórios de {(props.name)?.split(' ')[0]}</Title>
             <ListOfReposContainer>
                 {(props?.repos).map(repo => (
-                    <Repo onClick={() => window.location.href(repo?.html_url)} key={repo?.id}>
+                    <Repo onClick={() => window.location.assign(repo?.html_url)} key={repo?.id}>
                         <h2>{repo?.name}</h2>
                         <p>{repo?.description}</p>
                     </Repo>
